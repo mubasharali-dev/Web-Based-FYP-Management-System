6d42f38604ed2ef3c659d3d4411c9c7ca672817d
Mubashar Ali <mubashar.hashmat@gmail.com>
2023-03-04 13:06:08 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AdminClasses from "./Components/AdminClasses";
+import { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
+  const [classDetails, setClassDetails] = useState({
+    // className: "Class A",
+    totalStudents: 47,
+    totalProjects: 17,
+    totalSupervisors: 4,
+  });
+
+  const handleDelete = () => {
+    // handle delete functionality here
+  };
+
+  const handlePass = () => {
+    // handle passed functionality here
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <AdminClasses
+            className={classDetails.className}
+            totalStudents={classDetails.totalStudents}
+            totalProjects={classDetails.totalProjects}
+            totalSupervisors={classDetails.totalSupervisors}
+            onDelete={handleDelete}
+            onPass={handlePass}
+          />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
